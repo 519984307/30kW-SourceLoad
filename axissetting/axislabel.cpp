0ca1ede306fa98a8ce58e28883de0b38993b0ae1
chenbei97 <chenbei_electric@163.com>
2023-01-17 16:38:44 +0800

    坐标轴工具栏设置--轴标签设置

---
+++ axissetting/axislabel.cpp
@@ -0,0 +1,117 @@
+#include <axissetting/axislabel.h>
+
+AxisLabel::AxisLabel(QChart * chart,QAbstractAxis*axis,const QIcon&icon, QWidget*parent):
+    AxisBox(chart,axis,icon,parent)
+{ // visible,color,brush,font,angle
+
+    mAxisLabelVisibility = new QCheckBox;
+    updateVisibilityState();
+
+    mAxisLabelAngle = new GradientParamSpinBox;
+    mAxisLabelAngle->setRange(0.,360.);
+    mAxisLabelAngle->setSuffix("°");
+    updateAngleState();
+
+    mAxisLabelColor = new QPushButton(tr("设置颜色"));
+    updateColorState();
+
+    //mAxisLabelBrush = brushStyleCombo();
+    //updateBrushState();
+
+    mAxisLabelFont = new QPushButton(tr("设置格式"));
+    updateFontState();
+
+    QFormLayout* lay = new QFormLayout;
+    lay->addRow(tr("&标签可见"),mAxisLabelVisibility);
+    lay->addRow(tr("&标签角度"),mAxisLabelAngle);
+    lay->addRow(tr("&标签颜色"),mAxisLabelColor);
+    lay->addRow(tr("&标签格式"),mAxisLabelFont);
+    //lay->addRow(tr("&标签画刷"),mAxisLabelBrush);// 画刷对文字没有意义,没必要加
+
+    setTitle(tr("轴标签"));
+    setLayout(lay);
+}
+
+
+void AxisLabel::updateState()
+{
+    updateVisibilityState();
+    updateAngleState();
+    updateColorState();
+    //updateBrushState();
+    updateFontState();
+}
+
+void AxisLabel::updateVisibilityState()
+{
+    Qt::CheckState state = mCurrentAxis->labelsVisible()?Qt::Checked:Qt::Unchecked;
+    mAxisLabelVisibility->setCheckState(state);
+
+    disconnect(mAxisLabelVisibility,&QCheckBox::stateChanged,this,&AxisLabel::changeVisibility);
+    connect(mAxisLabelVisibility,&QCheckBox::stateChanged,this,&AxisLabel::changeVisibility);
+}
+
+void AxisLabel::changeVisibility(int state)
+{
+    bool s = state == Qt::Checked;
+    mCurrentAxis->setLabelsVisible(s);
+}
+
+void AxisLabel::updateAngleState()
+{
+    mAxisLabelAngle->setValue((double)mCurrentAxis->labelsAngle());
+
+    disconnect(mAxisLabelAngle,SIGNAL(valueChanged(double)),this,SLOT(changeAngle(double)));
+    connect(mAxisLabelAngle,SIGNAL(valueChanged(double)),this,SLOT(changeAngle(double)));
+}
+
+void AxisLabel::changeAngle(double angle)
+{
+    mCurrentAxis->setLabelsAngle((int)angle);
+}
+
+void AxisLabel::updateColorState()
+{
+    disconnect(mAxisLabelColor,&QPushButton::clicked,this,&AxisLabel::changeColor);
+    connect(mAxisLabelColor,&QPushButton::clicked,this,&AxisLabel::changeColor);
+}
+
+void AxisLabel::changeColor()
+{
+    QColor oldcolor = mCurrentAxis->labelsColor();
+    QColorDialog * dlg = colorDialog(oldcolor);
+    connect(dlg,static_cast<void (QColorDialog::*) (const QColor&)>(&QColorDialog::colorSelected)
+           ,this,[=](const QColor& color){ mCurrentAxis->setLabelsColor(color);
+    });
+    dlg->exec(); delete dlg;
+}
+
+void AxisLabel::updateFontState()
+{
+    disconnect(mAxisLabelFont,&QPushButton::clicked,this,&AxisLabel::changeFont);
+    connect(mAxisLabelFont,&QPushButton::clicked,this,&AxisLabel::changeFont);
+}
+
+void AxisLabel::changeFont()
+{
+    QFont oldfont = mCurrentAxis->labelsFont();
+    QFontDialog * dlg = fontDialog(oldfont);
+    connect(dlg,static_cast<void (QFontDialog::*) (const QFont&)>(&QFontDialog::fontSelected)
+           ,this,[=](const QFont& font){mCurrentAxis->setLabelsFont(font);});
+    dlg->exec(); delete dlg;
+}
+
+void AxisLabel::updateBrushState()
+{
+    mAxisLabelBrush->setCurrentIndex(mCurrentAxis->titleBrush().style());
+
+    disconnect(mAxisLabelBrush,SIGNAL(currentIndexChanged(int)),this,SLOT(changeBrush(int)));
+    connect(mAxisLabelBrush,SIGNAL(currentIndexChanged(int)),this,SLOT(changeBrush(int)));
+}
+
+void AxisLabel::changeBrush(int index)
+{
+    QBrush brush = mCurrentAxis->labelsBrush();
+    brush.setStyle(Qt::BrushStyle(index));
+    mCurrentAxis->setLabelsBrush(brush);
+}
---
+++ axissetting/axislabel.h
@@ -0,0 +1,36 @@
+#ifndef AXISLABEL_H
+#define AXISLABEL_H
+
+#include <axissetting/axisbox.h>
+#include <chartsetting/gradientparamspinbox.h>
+#include <QFormLayout>
+#include <QPushButton>
+#include <QCheckBox>
+
+
+class AxisLabel : public AxisBox
+{
+    Q_OBJECT
+public:
+    explicit AxisLabel(QChart*, QAbstractAxis*,const QIcon&, QWidget*parent = Q_NULLPTR);
+    void updateState() override;
+private:
+    QCheckBox * mAxisLabelVisibility;
+    GradientParamSpinBox * mAxisLabelAngle;// 借助内部重载的wheelEvent
+    QPushButton * mAxisLabelColor;
+    QComboBox * mAxisLabelBrush;
+    QPushButton * mAxisLabelFont;
+    void updateVisibilityState();
+    void updateAngleState();
+    void updateColorState();
+    void updateBrushState();
+    void updateFontState();
+private slots:
+    void changeVisibility(int);
+    void changeAngle(double);
+    void changeColor();
+    void changeBrush(int);
+    void changeFont();
+};
+
+#endif // AXISLABEL_H
