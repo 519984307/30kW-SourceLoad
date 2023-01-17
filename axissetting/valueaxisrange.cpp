4e42879f49aecb8236ec971c70cc5f8479f316f8
chenbei97 <chenbei_electric@163.com>
2023-01-17 16:40:19 +0800

    针对线性坐标轴工具栏设置

---
+++ axissetting/valueaxisrange.cpp
@@ -0,0 +1,12 @@
+#include <axissetting/valueaxisrange.h>
+
+ValueAxisRange::ValueAxisRange(QChart * chart,QAbstractAxis*axis,const QIcon&icon, QWidget*parent):
+    AxisRange(chart,axis,icon,parent)
+{
+
+}
+
+void ValueAxisRange::updateState()
+{
+
+}
