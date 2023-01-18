de808fc7caa253fb2321d8ea48f16236bc527b83
chenbei97 <chenbei_electric@163.com>
2023-01-18 11:50:02 +0800

    增加坐标轴-轴阴影设置功能

--- axissetting/axisshade.cpp
+++ axissetting/axisshade.cpp
@@ -1,6 +1,120 @@
-#include "axisshade.h"
+#include <axissetting/axisshade.h>
 
-AxisShade::AxisShade()
+AxisShade::AxisShade(QChart * chart,QAbstractAxis*axis,const QIcon&icon, QWidget*parent):
+    AxisBox(chart,axis,icon,parent)
 {
+        mAxisShadeVisibility = new QCheckBox;
+        updateVisibilityState();
 
+        mAxisShadeBorderColor = new QPushButton(tr("设置边框颜色"));
+        updateBorderColorState();
+
+        mAxisShadeBrushColor = new QPushButton(tr("设置背景颜色"));
+        updateBrushColorState();
+
+        mAxisShadePenWidth = new QSpinBox;
+        updatePenWidthState();
+
+        mAxisShadeBrushStyle = brushStyleCombo();
+        updateBrushStyleState();
+
+        QFormLayout* lay = new QFormLayout;
+        lay->addRow(tr("&阴影可见"),mAxisShadeVisibility);
+        lay->addRow(tr("&边框颜色"),mAxisShadeBorderColor);
+        lay->addRow(tr("&边框宽度"),mAxisShadePenWidth);
+        lay->addRow(tr("&背景颜色"),mAxisShadeBrushColor);
+        lay->addRow(tr("&背景样式"),mAxisShadeBrushStyle);
+        setTitle(tr("轴阴影"));
+        setLayout(lay);
 }
+
+void AxisShade::updateState()
+{
+    updateVisibilityState();
+    updateBorderColorState();
+    updateBrushColorState();
+    updateBrushStyleState();
+    updatePenWidthState();
+}
+
+void AxisShade::updateVisibilityState()
+{
+    Qt::CheckState state = mCurrentAxis->shadesVisible()?Qt::Checked:Qt::Unchecked;
+    mAxisShadeVisibility->setCheckState(state);
+
+    disconnect(mAxisShadeVisibility,&QCheckBox::stateChanged,this,&AxisShade::changeVisibility);
+    connect(mAxisShadeVisibility,&QCheckBox::stateChanged,this,&AxisShade::changeVisibility);
+}
+
+void AxisShade::changeVisibility(int state)
+{
+    bool s = state == Qt::Checked;
+    mCurrentAxis->setShadesVisible(s);
+}
+
+void AxisShade::updateBorderColorState()
+{
+    disconnect(mAxisShadeBorderColor,&QPushButton::clicked,this,&AxisShade::changeBorderColor);
+    connect(mAxisShadeBorderColor,&QPushButton::clicked,this,&AxisShade::changeBorderColor);
+}
+
+void AxisShade::changeBorderColor()
+{
+    QColor oldcolor = mCurrentAxis->shadesBorderColor(); // 和shadesPen().brush().color()等价,画笔颜色就是边框颜色,画笔的画刷样式对边框没有意义
+    QColorDialog * dlg = colorDialog(oldcolor);
+    connect(dlg,static_cast<void (QColorDialog::*) (const QColor&)>(&QColorDialog::colorSelected)
+           ,this,[=](const QColor& color){ mCurrentAxis->setShadesBorderColor(color);
+    });
+    dlg->exec(); delete dlg;
+}
+
+void AxisShade::updateBrushColorState()
+{
+    disconnect(mAxisShadeBrushColor,&QPushButton::clicked,this,&AxisShade::changeBrushColor);
+    connect(mAxisShadeBrushColor,&QPushButton::clicked,this,&AxisShade::changeBrushColor);
+}
+
+void AxisShade::changeBrushColor()
+{
+    QColor oldcolor = mCurrentAxis->shadesColor(); // 和shadesBrush().color()等价,画刷颜色就是背景颜色
+    QColorDialog * dlg = colorDialog(oldcolor);
+    connect(dlg,static_cast<void (QColorDialog::*) (const QColor&)>(&QColorDialog::colorSelected)
+           ,this,[=](const QColor& color){ mCurrentAxis->setShadesColor(color);
+    });
+    dlg->exec(); delete dlg;
+}
+
+void AxisShade::updateBrushStyleState()
+{
+    mAxisShadeBrushStyle->setCurrentIndex(mCurrentAxis->shadesBrush().style());
+
+    disconnect(mAxisShadeBrushStyle,SIGNAL(currentIndexChanged(int)),this,SLOT(changeBrushStyle(int)));
+    connect(mAxisShadeBrushStyle,SIGNAL(currentIndexChanged(int)),this,SLOT(changeBrushStyle(int)));
+}
+
+void AxisShade::changeBrushStyle(int index)
+{
+    QBrush brush = mCurrentAxis->shadesBrush();
+    brush.setStyle(Qt::BrushStyle(index));
+    mCurrentAxis->setShadesBrush(brush);
+}
+
+void AxisShade::updatePenWidthState()
+{
+    mAxisShadePenWidth->setValue(mCurrentAxis->shadesPen().width());
+
+    //disconnect(mAxisShadePenWidth,SIGNAL(valueChanged(int)),this,SLOT(changePenWidth(int)));
+    //connect(mAxisShadePenWidth,SIGNAL(valueChanged(int)),this,SLOT(changePenWidth(int)));
+}
+
+void AxisShade::changePenWidth(int width)
+{
+    QPen pen = mCurrentAxis->shadesPen();
+    pen.setWidth(width);
+    mCurrentAxis->setShadesPen(pen);
+}
+
+
+
+
+
--- axissetting/axisshade.h
+++ axissetting/axisshade.h
@@ -1,11 +1,35 @@
 #ifndef AXISSHADE_H
 #define AXISSHADE_H
 
+#include <axissetting/axisbox.h>
+#include <QFormLayout>
+#include <QPushButton>
+#include <QCheckBox>
+#include <QSpinBox>
 
-class AxisShade
+class AxisShade : public AxisBox
 {
+    Q_OBJECT
 public:
-    AxisShade();
+    explicit AxisShade(QChart*, QAbstractAxis*,const QIcon&, QWidget*parent = Q_NULLPTR);
+    void updateState() override;
+private:
+    QCheckBox * mAxisShadeVisibility;
+    QPushButton * mAxisShadeBorderColor;
+    QPushButton * mAxisShadeBrushColor;
+    QSpinBox * mAxisShadePenWidth;
+    QComboBox * mAxisShadeBrushStyle;
+    void updateVisibilityState();
+    void updateBorderColorState();
+    void updateBrushColorState();
+    void updateBrushStyleState();
+    void updatePenWidthState();
+private slots:
+    void changeVisibility(int);
+    void changeBorderColor();
+    void changeBrushColor();
+    void changeBrushStyle(int);
+    void changePenWidth(int);
 };
 
 #endif // AXISSHADE_H
