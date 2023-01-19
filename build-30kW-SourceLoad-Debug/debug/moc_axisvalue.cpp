e00dd833df37f73354505cb05df2a05fbe07b0d7
chenbei97 <chenbei_electric@163.com>
2023-01-19 11:15:24 +0800

    update debug

--- build-30kW-SourceLoad-Debug/debug/moc_axisvalue.cpp
+++ build-30kW-SourceLoad-Debug/debug/moc_axisvalue.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_AxisValue_t {
-    QByteArrayData data[1];
-    char stringdata0[10];
+    QByteArrayData data[7];
+    char stringdata0[86];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -31,10 +31,18 @@ struct qt_meta_stringdata_AxisValue_t {
     )
 static const qt_meta_stringdata_AxisValue_t qt_meta_stringdata_AxisValue = {
     {
-QT_MOC_LITERAL(0, 0, 9) // "AxisValue"
+QT_MOC_LITERAL(0, 0, 9), // "AxisValue"
+QT_MOC_LITERAL(1, 10, 9), // "changeMin"
+QT_MOC_LITERAL(2, 20, 0), // ""
+QT_MOC_LITERAL(3, 21, 9), // "changeMax"
+QT_MOC_LITERAL(4, 31, 15), // "changeTickCount"
+QT_MOC_LITERAL(5, 47, 20), // "changeMinorTickCount"
+QT_MOC_LITERAL(6, 68, 17) // "changeLabelFormat"
 
     },
-    "AxisValue"
+    "AxisValue\0changeMin\0\0changeMax\0"
+    "changeTickCount\0changeMinorTickCount\0"
+    "changeLabelFormat"
 };
 #undef QT_MOC_LITERAL
 
@@ -44,26 +52,48 @@ static const uint qt_meta_data_AxisValue[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       0,    0, // methods
+       5,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
        0,       // signalCount
 
+ // slots: name, argc, parameters, tag, flags
+       1,    1,   39,    2, 0x08 /* Private */,
+       3,    1,   42,    2, 0x08 /* Private */,
+       4,    1,   45,    2, 0x08 /* Private */,
+       5,    1,   48,    2, 0x08 /* Private */,
+       6,    0,   51,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void, QMetaType::Double,    2,
+    QMetaType::Void, QMetaType::Double,    2,
+    QMetaType::Void, QMetaType::Int,    2,
+    QMetaType::Void, QMetaType::Int,    2,
+    QMetaType::Void,
+
        0        // eod
 };
 
 void AxisValue::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
-    Q_UNUSED(_o);
-    Q_UNUSED(_id);
-    Q_UNUSED(_c);
-    Q_UNUSED(_a);
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        AxisValue *_t = static_cast<AxisValue *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->changeMin((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 1: _t->changeMax((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 2: _t->changeTickCount((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 3: _t->changeMinorTickCount((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 4: _t->changeLabelFormat(); break;
+        default: ;
+        }
+    }
 }
 
 const QMetaObject AxisValue::staticMetaObject = {
-    { &AxisBox::staticMetaObject, qt_meta_stringdata_AxisValue.data,
+    { &QGroupBox::staticMetaObject, qt_meta_stringdata_AxisValue.data,
       qt_meta_data_AxisValue,  qt_static_metacall, nullptr, nullptr}
 };
 
@@ -78,12 +108,23 @@ void *AxisValue::qt_metacast(const char *_clname)
     if (!_clname) return nullptr;
     if (!strcmp(_clname, qt_meta_stringdata_AxisValue.stringdata0))
         return static_cast<void*>(this);
-    return AxisBox::qt_metacast(_clname);
+    return QGroupBox::qt_metacast(_clname);
 }
 
 int AxisValue::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
 {
-    _id = AxisBox::qt_metacall(_c, _id, _a);
+    _id = QGroupBox::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 5)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 5;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 5)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 5;
+    }
     return _id;
 }
 QT_WARNING_POP
