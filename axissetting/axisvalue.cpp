d16f0119699b9de4a9d31af98f658328ba77cb75
chenbei97 <chenbei_electric@163.com>
2023-01-19 11:21:28 +0800

    针对QValueAxis特有的布局功能

--- axissetting/axisvalue.cpp
+++ axissetting/axisvalue.cpp
@@ -1,34 +1,121 @@
 #include <axissetting/axisvalue.h>
 
-AxisValue::AxisValue(QChart * chart,QAbstractAxis*axis,const QIcon&icon, QWidget*parent):
-    AxisBox(chart,axis,icon,parent)
+AxisValue::AxisValue(QChart * chart,QWidget*parent):
+    QGroupBox(parent),mChart(chart),mCurrentAxis(nullptr)
 {
-     // 针对QValueAxis使用,tickcount,labelformat,range,
-    mAxisRange = new QGroupBox(tr("轴范围"));
-    QFormLayout * rlay = new QFormLayout;
-    QDoubleSpinBox * minRange = new QDoubleSpinBox;
-    QDoubleSpinBox * maxRange = new QDoubleSpinBox;
-    rlay->addRow(tr("&上限"),minRange);
-    rlay->addRow(tr("&下限"),maxRange);
-    mAxisRange->setLayout(rlay);
+     // 针对QValueAxis使用minortickcount,tickcount,labelformat,range,
+    mMinRange = new QDoubleSpinBox;
+    mMaxRange = new QDoubleSpinBox;
+    mMinRange->setSingleStep(0.01);
+    mMaxRange->setSingleStep(0.01);
+    mMinRange->setRange(-(double)INT_MAX,(double)INT_MAX);
+    mMaxRange->setRange(-(double)INT_MAX,(double)INT_MAX);
+    mTickCount = new QSpinBox;
+    mMinorTickCount = new QSpinBox;
+    mLabelFormatEdit = new QLineEdit("%.1f");
+    mLabelFormatBtn = new QPushButton(tr("设置刻度格式"));
+    mTickCount->setMinimum(2); // 最小2,默认5
 
-    QVBoxLayout * lay = new QVBoxLayout;
-    lay->addWidget(mAxisRange);
+    QGridLayout * lay = new QGridLayout;
+    lay->addWidget(new QLabel(tr("轴下限值")),0,0);
+    lay->addWidget(mMinRange,0,1);
+    lay->addWidget(new QLabel(tr("轴上限值")),1,0);
+    lay->addWidget(mMaxRange,1,1);
+    lay->addWidget(new QLabel(tr("轴主刻度个数")),2,0);
+    lay->addWidget(mTickCount,2,1);
+    lay->addWidget(new QLabel(tr("轴次刻度个数")),3,0);
+    lay->addWidget(mMinorTickCount,3,1);
+    lay->addWidget(mLabelFormatEdit,4,0);
+    lay->addWidget(mLabelFormatBtn,4,1);
+    lay->addWidget(new QLabel(tr("支持格式字符串(d,i,o,x,X,f,F,e,E,g,G,c)等")),5,0,1,2);
+
     setLayout(lay); // 布局不影响先初始化完毕
-
-    // 信号槽函数的连接根据是否QValueAxis类型来初始化
-    if (axis->type() != QAbstractAxis::AxisTypeValue) return;
-    mValueAxis = static_cast<QValueAxis*>(mCurrentAxis);
-    initConnections();
+    setMinimumWidth(450); // 跟随axisbox.cpp
+    setTitle(tr("其它"));
 }
 
-void AxisValue::initConnections()
+void AxisValue::setCurrentAxis(QValueAxis * axis)
 {
-
+    mCurrentAxis = axis;
+    updateState();
 }
 
+QValueAxis * AxisValue::currentAxis() const
+{
+    return mCurrentAxis;
+}
 
 void AxisValue::updateState()
 {
-
+    updateMinState();
+    updateMaxState();
+    updateTickCountState();
+    updateMinorTickCountState();
+    updateLabelFormatState();
 }
+
+void AxisValue::updateMinState()
+{
+        mMinRange->setValue(mCurrentAxis->min());
+
+        disconnect(mMinRange,SIGNAL(valueChanged(double)),this,SLOT(changeMin(double)));
+        connect(mMinRange,SIGNAL(valueChanged(double)),this,SLOT(changeMin(double)));
+}
+
+void AxisValue::changeMin(double min)
+{
+        mCurrentAxis->setMin(min);
+}
+
+void AxisValue::updateMaxState()
+{
+        mMaxRange->setValue(mCurrentAxis->max());
+
+        disconnect(mMaxRange,SIGNAL(valueChanged(double)),this,SLOT(changeMax(double)));
+        connect(mMaxRange,SIGNAL(valueChanged(double)),this,SLOT(changeMax(double)));
+}
+
+void AxisValue::changeMax(double max)
+{
+        mCurrentAxis->setMax(max);
+}
+
+void AxisValue::updateTickCountState()
+{
+       mTickCount->setValue(mCurrentAxis->tickCount());
+
+       disconnect(mTickCount,SIGNAL(valueChanged(int)),this,SLOT(changeTickCount(int)));
+       connect(mTickCount,SIGNAL(valueChanged(int)),this,SLOT(changeTickCount(int)));
+}
+
+void AxisValue::changeTickCount(int count)
+{
+       mCurrentAxis->setTickCount(count);
+}
+
+void AxisValue::updateMinorTickCountState()
+{
+       mMinorTickCount->setValue(mCurrentAxis->minorTickCount());
+
+       disconnect(mMinorTickCount,SIGNAL(valueChanged(int)),this,SLOT(changeMinorTickCount(int)));
+       connect(mMinorTickCount,SIGNAL(valueChanged(int)),this,SLOT(changeMinorTickCount(int)));
+}
+
+void AxisValue::changeMinorTickCount(int count)
+{
+       mCurrentAxis->setMinorTickCount(count);
+}
+
+void AxisValue::updateLabelFormatState()
+{
+        mLabelFormatEdit->setText(mCurrentAxis->labelFormat());
+
+        disconnect(mLabelFormatBtn,SIGNAL(clicked()),this,SLOT(changeLabelFormat()));
+        connect(mLabelFormatBtn,SIGNAL(clicked()),this,SLOT(changeLabelFormat()));
+}
+
+void AxisValue::changeLabelFormat()
+{
+        mCurrentAxis->setLabelFormat(mLabelFormatEdit->text().simplified());
+}
+
--- axissetting/axisvalue.h
+++ axissetting/axisvalue.h
@@ -1,25 +1,47 @@
 #ifndef AXISVALUE_H
 #define AXISVALUE_H
 
-#include <axissetting/axisbox.h>
+#include <QGroupBox>
 #include <QFormLayout>
 #include <QValueAxis>
 #include <QDoubleSpinBox>
 #include <QComboBox>
+#include <QChart>
+#include <QLabel>
+#include <QValueAxis>
+#include <QLineEdit>
+#include <QPushButton>
+using namespace QtCharts;
 
 // 作为widget被axissettingvalue(layout)使用
-class AxisValue: public AxisBox
+class AxisValue: public QGroupBox
 {
     Q_OBJECT
 public:
-    explicit AxisValue(QChart*, QAbstractAxis*,const QIcon&, QWidget*parent = Q_NULLPTR);
+    explicit AxisValue(QChart*, QWidget*parent = Q_NULLPTR);
+    void setCurrentAxis(QValueAxis*);
+    QValueAxis* currentAxis() const;
+    void updateState();
 private:
-    void updateState() override;
-    void initConnections();
-    QGroupBox * mAxisRange;
-    QGroupBox * mAxisTickCount;
-    QGroupBox * mAxisLabelFormat;
-    QValueAxis * mValueAxis;
+    QChart * mChart;
+    QValueAxis * mCurrentAxis;
+    QDoubleSpinBox * mMinRange;
+    QDoubleSpinBox * mMaxRange;
+    QSpinBox * mTickCount;
+    QSpinBox * mMinorTickCount;
+    QLineEdit * mLabelFormatEdit;
+    QPushButton * mLabelFormatBtn;
+    void updateMinState();
+    void updateMaxState();
+    void updateTickCountState();
+    void updateMinorTickCountState();
+    void updateLabelFormatState();
+private slots:
+    void changeMin(double);
+    void changeMax(double);
+    void changeTickCount(int);
+    void changeMinorTickCount(int);
+    void changeLabelFormat();
 };
 
 #endif // AXISVALUE_H
